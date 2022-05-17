255ddb03fdbf5ec7e550da47f4bc156d6609c6b2
Benjamin Webb <40066515+webb-ben@users.noreply.github.com>
2022-05-17 09:16:53 -0400

    Move config is needed during entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -26,6 +26,13 @@ echo "START /entrypoint.sh"
 
 set +e
 
+if test -f "${WIS2BOX_API_CONFIG}"; then
+    echo "${WIS2BOX_API_CONFIG} already exists."
+else
+    echo "Creating ${WIS2BOX_API_CONFIG}."
+    cp /app/docker/pygeoapi-config.yml ${WIS2BOX_API_CONFIG}
+fi
+
 export PYGEOAPI_HOME=/pygeoapi
 export PYGEOAPI_CONFIG="${WIS2BOX_API_CONFIG}"
 export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"

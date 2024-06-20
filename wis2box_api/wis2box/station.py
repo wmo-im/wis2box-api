a8a3e00a09c63048296954a10bae973228d9a528
Maaike <mlimper@wmo.int>
2024-06-20 17:09:54 +0200

    Update station.py

--- wis2box_api/wis2box/station.py
+++ wis2box_api/wis2box/station.py
@@ -152,7 +152,8 @@ class Stations():
         LOGGER.info("Loading stations from backend")
 
         stations = {}
-
+        
+        channel = channel.replace('origin/a/wis2/', '')
         try:
             es = Elasticsearch(API_BACKEND_URL)
             nbatch = 50
